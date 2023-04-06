c6ee78e1c8c25b2c3c4868e1e319769863b0bb05
Piotr Sarna <sarna@chiselstrike.com>
2023-04-06 09:37:06 +0200

    init with log level from RUST_LOG

--- src/lib.rs
+++ src/lib.rs
@@ -24,3 +24,22 @@ impl<S: tracing::Subscriber> tracing_subscriber::Layer<S> for WorkerLayer {
         worker::console_log!("[{level}] [{target}] [{name}] {fields}");
     }
 }
+
+static INIT: std::sync::Once = std::sync::Once::new();
+
+pub fn init<D>(ctx: &worker::RouteContext<D>) {
+    use tracing_subscriber::prelude::*;
+
+    INIT.call_once(|| {
+        let level = ctx
+            .secret("RUST_LOG")
+            .map(|s| s.to_string())
+            .unwrap_or("info".to_string())
+            .parse::<tracing::Level>()
+            .unwrap_or(tracing::Level::INFO);
+        let subscriber = tracing_subscriber::registry()
+            .with(tracing_subscriber::fmt::layer())
+            .with(WorkerLayer::new(level));
+        tracing::subscriber::set_global_default(subscriber).unwrap();
+    });
+}
